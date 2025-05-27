e4b6bcec99284b71de91db85084a32d6b524077b
Olyad T. <bmoobdi@gmail.com>
2025-05-27 23:17:29 +0300

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,12 +1,12 @@
 <?php
-$con = new mysqli(
-    getenv('DB_HOST'),
-    getenv('DB_USER'),
-    getenv('DB_PASSWORD'),
-    getenv('DB_NAME')
-);
-
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
+try {
+    $con = new PDO(
+        "pgsql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_NAME'),
+        getenv('DB_USER'),
+        getenv('DB_PASSWORD')
+    );
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
