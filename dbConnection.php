c6c2084f4022b20a3aeb6c93d63c5180fbad0872
Olyad T. <bmoobdi@gmail.com>
2025-05-27 23:13:42 +0300

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,3 +1,12 @@
 <?php
-$con= new mysqli('localhost','root','','quiz')or die("Could not connect to mysql".mysqli_error($con));
+$con = new mysqli(
+    getenv('DB_HOST'),
+    getenv('DB_USER'),
+    getenv('DB_PASSWORD'),
+    getenv('DB_NAME')
+);
+
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
 ?>
